93ad27298f08cd2209a19b80d2fa770849e135c1
Ramiza <119511952+Ramiza1413@users.noreply.github.com>
2024-07-26 11:21:40 +0530

    Create FindDuplicate.py

---
+++ Day1/FindDuplicate.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        nums.sort()
+        for i  in range(len(nums)):
+            if nums[i] == nums[i+1]:
+                return nums[i]
+        return none
